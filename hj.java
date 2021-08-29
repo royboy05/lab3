9da44fc1145abd48bb092682545b32f3dabc94ff
royboy05 <89238405+royboy05@users.noreply.github.com>
2021-08-29 23:03:58 +0530

    first commit

---
+++ hj.java
@@ -0,0 +1,15 @@
+public class hj { 
+    public static void main(String args[]) 
+    { 
+        int a[]; 
+        int sum=0; 
+        a=new int[10]; 
+        for(int i=0;i<10;i++) 
+         { 
+           a[i]=Integer.parseInt(args[i]); 
+           a[i]=a[i]*a[i]; 
+           sum=sum+a[i]; 
+         } 
+        System.out.println(sum); 
+    } 
+} 
